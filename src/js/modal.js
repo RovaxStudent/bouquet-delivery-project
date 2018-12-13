28241bee87a00ea948ba15b88f40bb52d3ee2b17
Alex Rovcov <rovax@live.ru>
2018-12-14 00:05:06 +0200

    Modals

---
+++ src/js/modal.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$(".header-contacts__action").on("click", function(){
+		$('.overlay-contact').css('display', 'flex')
+	});
+	$(".questions-order").on("click", function(){
+		$('.overlay-contact').css('display', 'flex')
+	});
+	$(".flower-1").on("click", function(){
+		$('.flower_1').css('display', 'flex')
+	});
+	$(".flower-2").on("click", function(){
+		$('.flower_2').css('display', 'flex')
+	});
+	$(".flower-3").on("click", function(){
+		$('.flower_3').css('display', 'flex')
+	});
+	$(".flower-4").on("click", function(){
+		$('.flower_4').css('display', 'flex')
+	});
+	$(".flower-5").on("click", function(){
+		$('.flower_5').css('display', 'flex')
+	});
+	$(".flower-6").on("click", function(){
+		$('.flower_6').css('display', 'flex')
+	});
+	$(".popup-close").on("click", function(){
+		$(".overlay").hide()
+	});
+});
---
+++ src/mailer/smart.php
@@ -0,0 +1,46 @@
+<?php 
+
+// Принимаем данные от пользователя - переменные полей ввода
+$phone = $_POST['user_phone'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+
+// SMTP — простой протокол передачи почты Simple Mail Transfer Protocol
+// SMTP сервер отвечает за отправку почтовых рассылок
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'lthinking@mail.ru';                 // Наш логин
+$mail->Password = '869942467a';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('lthinking@mail.ru', 'User');   // От кого письмо 
+$mail->addAddress('rovax@live.ru');      // Добавить получателя(их может быть несколько)
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Это тема сообщения';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Телефон: ' . $phone . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+    return false;
+} else {
+		// Куда попадет пользователь после отправки данных
+    return true;
+}
+
+?>
